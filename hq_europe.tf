f97895bfc19b562c00fa1243d9dbf3890760f444
kilik42 <marvinevins@yahoo.com>
2024-05-13 21:23:23 -0500

    working on target tags

--- hq_europe.tf
+++ hq_europe.tf
@@ -75,5 +75,5 @@ resource "google_compute_firewall" "allow_http_for_europe" {
   }
 
   source_ranges = ["0.0.0.0/0", "35.235.240.0/20"]
-  target_tags  = ["europe","asia","america"]
+  target_tags  = ["america-http-server","iap-ssh-allowed"]
 }
